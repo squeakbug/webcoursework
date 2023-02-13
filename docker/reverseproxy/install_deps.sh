489184f2eb1ea16ed0f22f3f6be366dbd5893df3
squeakbug <artyum.lisnevskiy@yandex.ru>
2023-02-13 04:41:02 +0300

    add http/2 support

--- docker/reverseproxy/install_deps.sh
+++ docker/reverseproxy/install_deps.sh
@@ -16,4 +16,19 @@ apt-get install -y \
 	nginx \
 	binutils-dev \
 	net-tools \
-	nginx-extras \
+	nginx-extras 
+
+mkdir -p /etc/ssl/private
+chmod 700 /etc/ssl/private
+
+touch ssl_data
+echo "RU" > ssl_data
+echo "Volodya" >> ssl_data
+echo "Moscow" >> ssl_data
+echo "BMSTU" >> ssl_data
+echo "ICS" >> ssl_data
+echo "localhost" >> ssl_data
+echo "volodya@mail.ru" >> ssl_data
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt < ssl_data
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
