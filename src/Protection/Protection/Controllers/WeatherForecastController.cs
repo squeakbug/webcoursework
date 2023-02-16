c283e2b15f16cdd082a2218a53171d4d8f97f472
squeakbug <artyum.lisnevskiy@yandex.ru>
2023-02-16 12:49:25 +0300

    add serial id post method

--- src/Protection/Protection/Controllers/WeatherForecastController.cs
+++ src/Protection/Protection/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using System.Collections.Generic;
+using System;
 
 namespace Protection.Controllers
 {
@@ -8,28 +9,47 @@ namespace Protection.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         private static List<WeatherForecast> Data = new List<WeatherForecast>(); 
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            _logger = logger ?? throw new Exception("Null logger");
         }
 
         [HttpPut, Route("forecasts/{forecastId}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public void UpdateForecast(int forecastId, [FromBody] WeatherForecast forecast)
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(WeatherForecast))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult UpdateForecast(int forecastId, [FromBody] WeatherForecast forecast)
         {
             var innerForecastIndx = Data.FindIndex(x => x.Id == forecastId);
             if (innerForecastIndx != -1)
             {
                 Data[innerForecastIndx] = forecast;
+                return Ok(forecast);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        private int GetNewListId(List<WeatherForecast> data)
+        {
+            if (data.Count == 0)
+                return 0;
+            else
+            {
+                int maxId = data[0].Id;
+                foreach (var item in data)
+                {
+                    if (item.Id > maxId)
+                    {
+                        maxId = item.Id;
+                    }
+                }
+                return maxId + 1;
             }
         }
 
@@ -37,14 +57,20 @@ namespace Protection.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public void AddForecast([FromBody] WeatherForecast forecast)
         {
+            forecast.Id = GetNewListId(Data);
             Data.Add(forecast);
         }
 
         [HttpGet, Route("forecasts/{forecastId}")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(WeatherForecast))]
-        public WeatherForecast Get(int forecastId)
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int forecastId)
         {
-            return Data.Find(wf => wf.Id == forecastId);
+            var d = Data.Find(wf => wf.Id == forecastId);
+            if (d == null)
+                return Ok(d);
+            else
+                return NotFound();
         }
 
         [HttpGet, Route("forecasts")]
